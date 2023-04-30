ecdcff0a8973abf388c4daa4441b85b4ddb1ad1e
Wes Henderson <whendersonii@gmail.com>
2023-04-29 20:26:57 -0500

    Adding the breathe effect.

--- lights.py
+++ lights.py
@@ -1,11 +1,39 @@
 #!/usr/bin/env python3
 
 class Lights:
-    def toggle(light):
+    def toggle(light_id):
         import requests
         import json
         from auth import Auth
 
         auth = Auth.auth()
 
-        response = requests.post(f"https://api.lifx.com/v1/lights/{light}/toggle", headers=auth)
+        response = requests.post(f"https://api.lifx.com/v1/lights/{light_id}/toggle", headers=auth)
+
+    def breathe_effect(light_id, color):
+        import requests
+        import json
+        from auth import Auth
+
+        auth = Auth.auth()
+
+        data = {
+            "period": 2,
+            "cycles": 10,
+            "color": f"{color}",
+        }
+
+        response = requests.post(f"https://api.lifx.com/v1/lights/{light_id}/effects/breathe", data=data, headers=auth)
+
+    def stop_effect(light_id):
+        import requests
+        import json
+        from auth import Auth
+
+        auth = Auth.auth()
+
+        data = {
+            "power_off": True
+        }
+
+        response = requests.post(f"https://api.lifx.com/v1/lights/{light_id}/effects/off", data=data, headers=auth)
