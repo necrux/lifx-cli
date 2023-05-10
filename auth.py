7dbcf939711dd27fee9c73f89a6b985782518de4
Wes Henderson <whendersonii@gmail.com>
2023-05-10 17:06:11 -0500

    Allowing for an env var override.

--- auth.py
+++ auth.py
@@ -4,11 +4,15 @@ class Auth:
     def auth(self):
         """Returns the authentication headers to authenticate to LIFX."""
         import configparser
-        
-        config = configparser.ConfigParser()
-        config.read('/home/wes/.keys')
-        
-        token = config['lifx']['token']
+        from os import environ, getenv
+
+        if environ.get('LIFX'):
+            token = getenv('LIFX')
+        else:
+            config = configparser.ConfigParser()
+            config.read('/home/wes/.keys')
+
+            token = config['lifx']['token']
         
         headers = {
             "Authorization": "Bearer %s" % token,
