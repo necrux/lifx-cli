e4f411cdcff5c068d2c2ecfa581ec194d966273a
Wes Henderson <whendersonii@gmail.com>
2023-05-11 17:25:33 -0500

    Adding a CLI option to configure the auth file.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# lifx-cli
+# The Unofficial LIFX CLI
 Playing around with the LifX API with the goal of creating a CLI.
+
+## Authentication
+Authentication is done using your token which is stored in `~/.keys`. This can be configured by running `lifx --configure`. 
+
+**Example `~/.keys` *ini* file:**
+
+```
+[lifx]
+token = FAKE_TOKEN
+```
+
+Alternatively, you can override this value by setting the `LIFX` environment variable:
+
+```
+export LIFX=FAKE_TOKEN
+```
+
+Refer to the [documentation](https://api.developer.lifx.com/reference/authentication) in order to generate your own token.
--- auth.py
+++ auth.py
@@ -1,21 +1,60 @@
 #!/usr/bin/env python3
 
 class Auth:
+
+    def __init__(self):
+        from os import path
+
+        self.auth_file = f'{path.expanduser("~")}/.keys'
+        self.auth_file_section = 'lifx'
+        self.auth_file_key = 'token'
+        self.auth_env_var = 'LIFX'
+
+    def configure(self):
+        """Configure/update the ini file for API authentication."""
+        import configparser
+        from os import path
+
+        config = configparser.RawConfigParser()
+        api_key = input('What is your API token?: ')
+
+        if path.exists(self.auth_file):
+            config.read(self.auth_file)
+
+        if not config.has_section(self.auth_file_section):
+            config = configparser.RawConfigParser()
+            config.add_section(self.auth_file_section)
+            config.set(self.auth_file_section, self.auth_file_key, api_key)
+
+            with open(self.auth_file, 'a+') as f:
+                config.write(f)
+        else:
+            overwrite = input('You already have an API key saved in ~/.keys. Do you want to overwrite this value? y/N ')
+            overwrite = overwrite.lower()
+
+            if overwrite == 'y':
+                config.set(self.auth_file_section, self.auth_file_key, api_key)
+
+                with open(self.auth_file, 'w') as f:
+                    config.write(f)
+
+        return
+
     def auth(self):
-        """Returns the authentication headers to authenticate to LIFX."""
+        """Returns the headers required to authenticate to the LIFX API."""
         import configparser
         from os import environ, getenv
 
-        if environ.get('LIFX'):
-            token = getenv('LIFX')
+        if environ.get(self.auth_env_var):
+            token = getenv(self.auth_env_var)
         else:
             config = configparser.ConfigParser()
-            config.read('/home/wes/.keys')
+            config.read(self.auth_file)
 
-            token = config['lifx']['token']
-        
+            token = config[self.auth_file_section][self.auth_file_key]
+
         headers = {
-            "Authorization": "Bearer %s" % token,
+            'Authorization': f'Bearer {token}',
         }
 
         return headers
