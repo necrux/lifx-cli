22a6493c4876fd1d3e427346d0b57420a5b453ed
Wes Henderson <whendersonii@gmail.com>
2023-05-11 22:31:07 -0500

    Adding pulse effect and the ability to specify multiple colors.

--- lights.py
+++ lights.py
@@ -44,16 +44,52 @@ class Lights:
         auth = Auth()
         auth_headers = auth.auth()
 
-        data = {
-            "period": 2,
-            "cycles": 10,
-            "color": f"{color}",
-        }
+        if len(color) == 1:
+            data = {
+                "period": 2,
+                "cycles": 10,
+                "color": f"{color[0]}",
+            }
+        else:
+            data = {
+                "period": 2,
+                "cycles": 10,
+                "from_color": f"{color[0]}",
+                "color": f"{color[1]}",
+            }
 
         if group:
             light_id = f'group_id:{light_id}'
 
         requests.post(f"https://api.lifx.com/v1/lights/{light_id}/effects/breathe", data=data, headers=auth_headers)
+
+    def pulse_effect(self, light_id, group, color):
+        """Activates the pulse effect on the specified light (period: 2; cycles: 10). Requires the device ID and
+        color."""
+        import requests
+        from auth import Auth
+
+        auth = Auth()
+        auth_headers = auth.auth()
+
+        if len(color) == 1:
+            data = {
+                "period": 2,
+                "cycles": 10,
+                "color": f"{color[0]}",
+            }
+        else:
+            data = {
+                "period": 2,
+                "cycles": 10,
+                "from_color": f"{color[0]}",
+                "color": f"{color[1]}",
+            }
+
+        if group:
+            light_id = f'group_id:{light_id}'
+
+        requests.post(f"https://api.lifx.com/v1/lights/{light_id}/effects/pulse", data=data, headers=auth_headers)
 
     def stop_effect(self, light_id, group):
         """Stop all effects on the specified light. Requires Light ID."""
